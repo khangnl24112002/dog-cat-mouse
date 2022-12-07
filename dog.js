afee2c2726bf934f6bcde9e0bdd00a5d507b8414
20120302 <khangnl24112002@gmail.com>
2022-12-07 15:49:49 +0700

    Say Hi dog method

--- dog.js
+++ dog.js
@@ -1,7 +1,14 @@
-function Dog(){
+function Dog(name){
 	this.stomach = [];
+	this.name = name;
 }
 
 Dog.prototype.eat = function(cat){
 	this.stomach.push(cat);
 }
+
+Dog.prototype.sayHi = function(){
+	console.log('Hi! Iam a dog. My name is ' + this.name);
+}
+
+module.exports = Dog;
